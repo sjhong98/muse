790a7073cb0e5798049ec63d3edb5a107a5dd910
sjhong98 <sjhong98@icloud.com>
2023-12-11 16:28:26 +0900

    feat

---
+++ pages/modules.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import axios from 'axios';
+import { useEffect } from "react"
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+export const firstActive = (res : InferGetServerSidePropsType<GetServerSideProps>) => {
+    useEffect(() => {
+        axios.post('http://www.museum.go.kr/site/main/openapi/relic?pageSize=10')
+        .then((res) => {
+            console.log("res : ", res);
+        })
+        .catch(err => {
+            console.log(err);
+        }) 
+        // console.log(res)
+    }, [])
+}
